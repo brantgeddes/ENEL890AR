ff45745a07aadcee371e6d8705cd3c59466bfbe1
Brant Geddes <brantgeddes@gmail.com>
2020-06-28 21:04:26 -0600

    dft and read first rev finished

--- dft.py
+++ dft.py
@@ -1,49 +1,52 @@
-import math
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
+from message import decode_msg_size
+import os, select, math, numpy as np
+
+def get_message(fifo: int) -> str:
+    msg_size_bytes = os.read(fifo, 4)
+    msg_size = decode_msg_size(msg_size_bytes)
+    msg_content = os.read(fifo, msg_size).decode("utf8")
+    return msg_content
+
+def dft(batch):
+    # Empty arrays
+    real = np.array([])
+    imag = np.array([])
+
+    # Loop through all buckets
+    for k in range(N):
+        realV = 0
+        imagV = 0
+        # Perform a correlation for each bucket
+        for n in range(N):
+            realV += (1 / N) * batch[n] * math.cos(2 * math.pi * n * k / N)
+            imagV += (1 / N) * batch[n] * math.sin(2 * math.pi * n * k / N)
+        # Append correllation coefficients to the current bucket
+        real = np.append(real, realV) 
+        imag = np.append(imag, imagV)
+
+    return (real, imag)
 
 # Target sample rate
-sample_rate = 480
-# Data source path
-data_source = 'Q1Data.csv'
+sample_rate = 490
+# Batch size
+N = 64
+# Queue Named Pipe
+PIPE_NAME='ADC_READ_PIPE'
+os.mkfifo(PIPE_NAME)
 
-# Change data_source variable to load different dataset
-data = pd.read_csv(data_source, sep=",", skiprows=2, usecols=["Qn"]).to_numpy()
+try:
+    fifo = os.open(PIPE_NAME, os.O_RDONLY)
+except:
+    print(e)
+    exit()
 
-# Number of data points
-N = len(data)
-# Empty arrays
-real = np.array([])
-imag = np.array([])
-
-# Loop through all buckets
-for k in range(N):
-    realV = 0
-    imagV = 0
-    # Perform a correlation for each bucket
-    for n in range(N):
-        realV += (1 / N) * data[n] * math.cos(2 * math.pi * n * k / N)
-        imagV += (1 / N) * data[n] * math.sin(2 * math.pi * n * k / N)
-    # Append correllation coefficients to the current bucket
-    real = np.append(real, realV) 
-    imag = np.append(imag, imagV)
-
-print(real[0])
-# Lambdas to solve amplitude and phase
-amplitude = list(map(lambda x,y: 2 * (x**2 + y**2)**(1/2), real, imag))
-phase = list(map(lambda x,y: math.atan(y/x), real, imag))
-
-plt.subplot(3, 1, 1)
-plt.title('Function')
-plt.plot(range(N), data)
-# Scale DFT buckets to Fs/2 frequency range
-f_axis = list(range(int(N/2) + 1))
-f_axis = list(map(lambda x: (x/N) * sample_rate, f_axis))
-plt.subplot(3, 1, 2)
-plt.title('Amplitude')
-plt.plot(f_axis, amplitude[:int(N/2) + 1])
-plt.subplot(3, 1, 3)
-plt.title('Phase')
-plt.plot(f_axis, phase[:int(N/2) + 1])
-plt.show()
+try:
+    while True:
+        if fifo:
+            batch = fifo.get_message(fifo)
+            batch = dft(batch)
+            print(batch)
+except:
+    print(e)
+finally:
+    os.close(fifo)
--- read.py
+++ read.py
@@ -1,13 +1,13 @@
-import smbus, time
+import smbus, time, os
+from message import create_msg
 
 bus = smbus.SMBus(1)
 
 DEVICE_ADDRESS = 0x48
 TIME_PERIOD = 1/490
 
-#bus.write_i2c_block_data(DEVICE_ADDRESS, 0x01, [0x44, 0x43])
+PIPE_NAME='ADC_READ_PIPE'
 
-#print(bus.read_block_data(DEVICE_ADDRESS, 0x00))
 
 try:
     config = bus.read_i2c_block_data(DEVICE_ADDRESS, 0x01, 2)
@@ -18,10 +18,24 @@ try:
         bus.write_i2c_block_data(DEVICE_ADDRESS, 0x01, [0x44, 0x43]) 
 except:
     print(e)
+    exit()
 
-next_read = time.time() + TIME_PERIOD
-while True:
-    if time.time() > next_read:
-        next_read = time.time() + TIME_PERIOD
-        conversion = bus.read_i2c_block_data(DEVICE_ADDRESS, 0x00, 2)
-        print(conversion[0] + conversion[1])
+try:
+    fifo = os.open(PIPE_NAME, os.O_WRONLY)
+except:
+    print(e)
+    exit()
+
+try:
+    next_read = time.time() + TIME_PERIOD
+    while True:
+        if time.time() > next_read:
+            next_read = time.time() + TIME_PERIOD
+            conversion = bus.read_i2c_block_data(DEVICE_ADDRESS, 0x00, 2)
+            conversion = conversion[0] * 16**2 + conversion[1]
+            os.write(fifo, conversion)
+except:
+    print(e)
+finally:
+    os.close(fifo)
+
