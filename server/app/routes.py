5ad1bc1cb299045699098a807206d9a8e8f69baf
Brant Geddes <brantgeddes@gmail.com>
2020-06-29 11:30:19 -0600

    Finished server routes for dft and samples

--- server/app/routes.py
+++ server/app/routes.py
@@ -1,22 +1,21 @@
 from app import app
 import sqlite3
+from flask import jsonify
 
-conn = sqlite3.connect('/home/pi/data.db')
-
-@app.route('/')
-@app.route('/index')
-def index():
-    print('test')
-    return 'test'
+conn = sqlite3.connect('/home/pi/enel/data.db')
 
 @app.route('/samples')
 def samples():
-    for row in conn.execute('select * from samples'):
-        print(row)
-    return 'test'
+    values = []
+    for row in conn.execute('select value from samples'):
+        values.append(row[0])
+    return jsonify({ "values": values })
 
 @app.route('/dft')
 def dft():
-    for row in conn.execute('select * from dft'):
-        print(row)
-    return 'test'
+    real = []
+    imag = []
+    for row in conn.execute('select real, imag from dft'):
+        real.append(row[0])
+        imag.append(row[1])
+    return jsonify({ "real": real, "imag": imag })
