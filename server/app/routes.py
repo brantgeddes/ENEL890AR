546ec69313650880ab306df707e0724ecf44ddaf
Brant Geddes <brantgeddes@gmail.com>
2020-06-29 11:18:06 -0600

    sqlite written, going to test

--- server/app/routes.py
+++ server/app/routes.py
@@ -1,7 +1,22 @@
 from app import app
+import sqlite3
+
+conn = sqlite3.connect('/home/pi/data.db')
 
 @app.route('/')
 @app.route('/index')
 def index():
     print('test')
+    return 'test'
+
+@app.route('/samples')
+def samples():
+    for row in conn.execute('select * from samples'):
+        print(row)
+    return 'test'
+
+@app.route('/dft')
+def dft():
+    for row in conn.execute('select * from dft'):
+        print(row)
     return 'test'
